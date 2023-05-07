409f3df8433b727b684c46eae93d03b543e753b2
ykawabata17 <ai1063@nara.kosen-ac.jp>
2023-05-07 18:04:29 +0900

    fix directory

---
+++ Chapter4/ownership/target/debug/deps/ownership-8ae8d30dd860841a.d
@@ -0,0 +1,5 @@
+/Users/yuuuya/workspace/rust_practice/Chapter4/ownership/target/debug/deps/ownership-8ae8d30dd860841a: src/main.rs
+
+/Users/yuuuya/workspace/rust_practice/Chapter4/ownership/target/debug/deps/ownership-8ae8d30dd860841a.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter4/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/yuuuya/workspace/rust_practice/Chapter4/ownership/target/debug/ownership: /Users/yuuuya/workspace/rust_practice/Chapter4/ownership/src/main.rs
